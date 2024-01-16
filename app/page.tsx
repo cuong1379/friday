7a8415e6d53e2963883b42b651ba25e5bebdaa09
nguyen <cuong.nguyen@crossian.com>
2024-01-16 22:24:41 +0700

    initial landing page and go prd first time

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,22 @@
-import { Poppins } from "next/font/google";
-
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { LoginButton } from "@/components/auth/login-button";
-
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"],
-});
+import React from "react";
+import Description from "@/components/landing-page/description";
+import Features from "@/components/landing-page/features";
+import Footer from "@/components/landing-page/footer";
+import Header from "@/components/landing-page/header";
+import Hero from "@/components/landing-page/hero";
+import Introduce from "@/components/landing-page/introduce";
+import SocialMedia from "@/components/landing-page/social-media";
 
 export default function Home() {
   return (
-    <main
-      className="flex h-full flex-col items-center justify-center bg-ln-dark"
-    >
-      <div className="space-y-6 text-center">
-        <h1
-          className={cn(
-            "text-6xl font-semibold text-primary drop-shadow-md",
-            font.className
-          )}
-        >
-          FRIDAY
-        </h1>
-        <div>
-          <LoginButton asChild>
-            <Button variant="outline" size="lg">
-              Start
-            </Button>
-          </LoginButton>
-        </div>
-      </div>
+    <main className="relative flex min-h-screen flex-col justify-between bg-ln-dark text-white">
+      <Header />
+      <Hero />
+      <Introduce />
+      <Features />
+      <Description />
+      <Footer />
+      <SocialMedia />
     </main>
   );
 }
